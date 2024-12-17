33e58fad0134ad7263cafc1c3ca152d6cd46b66d
jd451 <jenniferannstrauss@gmail.com>
2024-12-17 15:21:42 -0800

    added comments

---
+++ complete.R
@@ -0,0 +1,22 @@
+complete <- function(directory, ident=1:332){
+  #Create empty data frame
+  df <- data.frame()
+  
+  #Populate data frame using a for loop
+  for (i in ident) {
+    #Get the row data
+    csv <- i
+    
+    name<- formatC(i, width=3, flag="0")
+    file <- paste(directory, '/', name, '.csv', sep='')
+    data <- read.csv(file)
+    completed <- data[complete.cases(data), ]
+    pollut <- nrow(completed)
+    #Populate the row
+    new.row <- data.frame(id = csv, nobs = pollut)
+    
+    #Add the row
+    df <- rbind(df, new.row)
+  }
+  print(df)
+}
--- pollutantmean.R
+++ pollutantmean.R
@@ -4,10 +4,13 @@ pollutantmean <- function(directory, pollutant, id = 1:332){
   entries <- 0
   name <- ""
   for (i in id) {
+    #keep leading zeroes in file name
     name<- formatC(i, width=3, flag="0")
     file <- paste(directory, '/', name, '.csv', sep='')
     data <- read.csv(file)
+    #Remove data except pollutant of interest
     poldata <-subset(data, select = pollutant)
+    #Remove NA entries
     entries <- entries + sum(!is.na(poldata))
     totalpol <- totalpol + sum(poldata, na.rm=TRUE)
     result = totalpol/entries
